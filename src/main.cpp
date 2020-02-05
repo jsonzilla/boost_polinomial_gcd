fea60a55464626d3687ea9ca2e4467761eeebf6c
Jeison <cardoso.jeison@gmail.com>
2020-02-05 15:49:03 -0300

    first try - not work

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,36 @@
 
-int main(int argc, char* argv[]) {
+#include<stdlib.h>
+
+#include <vector>
+
+#include "poly.h"
+
+using namespace std;
+
+const vector<double> GCD(vector<double>& p, vector<double>& q) {
+  vector<double> remainder;
+
+  while (1) {
+    remainder = Poly::Division(p, q);
+    if (remainder.size() == 0) {
+      break;
+    }
+    else {
+      p = q;
+      q = remainder;
+    }
+  }
+  return q;
+}
+
+int main() {
+  cout << "GDC\n";
+  vector<double> p = { 2,1,1 };// Poly::get_data();
+  vector<double> q = { 1,2,3 }; // Poly::get_data();
+  cout << "Polynomial1:";
+  Poly::display(p);
+  cout << "Polynomial2:";
+  Poly::display(p);
+  Poly::display(GCD(p, q));
   return 0;
 }
--- src/unit_tests.cpp
+++ src/unit_tests.cpp
@@ -1,8 +1,17 @@
 #include "gtest/gtest.h"
 
+#include "poly.h";
+
+#include <vector>
+
 TEST(example, demo)
 {
-  EXPECT_FALSE(2 == 3);
+  std::vector<double> a= { 2, 1, 1 };
+  std::vector<double> b= { 1, 2, 2, 3 };
+  std::vector<double> c = { 1, 3 };
+  auto v = Poly::Division(b, a);
+
+  EXPECT_EQ(v, c);
 }
 
 int main(int argc, char **argv) {
