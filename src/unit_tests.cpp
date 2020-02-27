e33a9a813ae2a2bf028e6d6f9e0cafd5fd6f5068
Jeison Cardoso <cardoso.jeison@gmail.com>
2020-02-27 18:32:44 -0300

    fixed

--- src/unit_tests.cpp
+++ src/unit_tests.cpp
@@ -1,17 +1,19 @@
 #include "gtest/gtest.h"
 
-#include "poly.h";
+#include <boost/math/tools/polynomial.hpp>
 
-#include <vector>
+using namespace boost::math;
+using namespace boost::math::tools; // for polynomial
 
-TEST(example, demo)
+TEST(poly, gdc_poly)
 {
-  std::vector<double> a= { 2, 1, 1 };
-  std::vector<double> b= { 1, 2, 2, 3 };
-  std::vector<double> c = { 1, 3 };
-  auto v = Poly::Division(b, a);
+  polynomial<double> a{ {10, -6, -4, 3} };
+  polynomial<double> b{ {-2.0, 1.0} };
 
-  EXPECT_EQ(v, c);
+  auto c = boost::math::tools::gcd(a, b);
+
+  polynomial<double> d{ {6.0} };
+  EXPECT_EQ(d, c);
 }
 
 int main(int argc, char **argv) {
